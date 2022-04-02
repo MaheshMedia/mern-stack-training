f0fe396728120a7d6d1b482bfba29c70e04e5375
Mahesh Naik <89764903+MaheshMedia@users.noreply.github.com>
2022-04-02 18:44:51 +0530

    Task completed

---
+++ arrays/main.js
@@ -0,0 +1,14 @@
+const arr=[1,3,5,7];
+console.log(arr);
+arr.push(9);
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.unshift(0);
+console.log(arr);
+arr.shift();
+console.log(arr);
+s=arr.join("");
+console.log(s);
+n=s.split("");
+console.log(n);
