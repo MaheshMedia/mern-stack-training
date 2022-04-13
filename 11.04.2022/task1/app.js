3d31778f8593c99d57edc22d549b8eff285d0a9e
Mahesh Naik <89764903+MaheshMedia@users.noreply.github.com>
2022-04-13 09:04:34 +0530

    task commited

---
+++ 11.04.2022/task1/app.js
@@ -0,0 +1,6 @@
+let form = document.getElementsByName("search");
+console.log(form);
+let firstInput = form[0].firstElementChild;
+console.log(firstInput);
+let lastInput = form[0].lastElementChild;
+console.log(lastInput);
